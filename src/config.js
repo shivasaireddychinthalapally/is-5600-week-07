e95e7f87e664ec13e2af0996c14d91cbf48413c8
shivasaireddychinthalapally <shivasaireddy.chinthalapally@slu.edu>
2025-05-07 06:02:27 +0000

    lab shiva 7

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // TODO: Replace this with your own Server URL for your codespace
-export const BASE_URL = "https://glowing-space-memory-q7p4jvr9g4p424rj7-3080.app.github.dev";
+export const BASE_URL = "https://bug-free-journey-wrxgvpjwj665h5rxp-3080.app.github.dev";
